2391efc84e44ec8af20391567af3348c017be716
Anton Lavrevov <lavrton@gmail.com>
2025-10-06 17:02:22 -0500

    scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { IBM_Plex_Sans } from 'next/font/google';
+import Script from 'next/script';
 
 const ibmPlexSans = IBM_Plex_Sans({
   subsets: ['latin'],
@@ -27,6 +28,23 @@ export default function Layout({ children }: LayoutProps<'/'>) {
         >
           {children}
         </RootProvider>
+        <Script
+          src="https://plausible.io/js/script.js"
+          data-domain="polotno.com"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="https://crawlchat.app/embed.js"
+          id="crawlchat-script"
+          data-id="67e312247a822a2303f2b8a7"
+          data-ask-ai="true"
+          data-ask-ai-background-color="rgba(79, 104, 232, 1)"
+          data-ask-ai-color="#ffffff"
+          data-ask-ai-text="💬 Ask AI"
+          data-ask-ai-position="br"
+          data-ask-ai-radius="20px"
+          strategy="lazyOnload"
+        />
       </body>
     </html>
   );
