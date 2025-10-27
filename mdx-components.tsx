d63a6f9d3bc7119ab37ca1f4478cde1978840e4b
Anton Lavrevov <lavrton@gmail.com>
2025-10-27 09:55:00 -0500

    CSS scoping

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,7 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import * as TabsComponents from 'fumadocs-ui/components/tabs';
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
 import SandpackDemo from './components/sandpack-demo-server';
 import CodeFromFile from './components/code-from-file';
 import Sandbox from './components/sandbox';
@@ -13,6 +14,8 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     SandpackDemo,
     CodeFromFile,
     Sandbox,
+    Accordion,
+    Accordions,
     ...components,
   };
 }
