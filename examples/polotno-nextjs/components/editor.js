d3c1375e31c4d3de7f43ba5698052806be087892
Anton Lavrevov <lavrton@gmail.com>
2025-12-01 12:20:09 -0500

    fix demo

--- examples/polotno-nextjs/components/editor.js
+++ examples/polotno-nextjs/components/editor.js
@@ -5,7 +5,6 @@ import { PagesTimeline } from 'polotno/pages-timeline';
 import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
 import { SidePanel } from 'polotno/side-panel';
 import { Workspace } from 'polotno/canvas/workspace';
-import 'polotno/polotno.blueprint.css';
 import { createStore } from 'polotno/model/store';
 
 const store = createStore({
@@ -18,17 +17,24 @@ const page = store.addPage();
 
 export const Editor = () => {
   return (
-    <PolotnoContainer style={{ width: '100vw', height: '100vh' }}>
-      <SidePanelWrap>
-        <SidePanel store={store} />
-      </SidePanelWrap>
-      <WorkspaceWrap>
-        <Toolbar store={store} downloadButtonEnabled />
-        <Workspace store={store} />
-        <ZoomButtons store={store} />
-        <PagesTimeline store={store} />
-      </WorkspaceWrap>
-    </PolotnoContainer>
+    <>
+      {/* use CDN for css, you can also use local css file */}
+      <link
+        href="https://unpkg.com/polotno@^2/blueprint.polotno.css"
+        rel="stylesheet"
+      />
+      <PolotnoContainer style={{ width: '100vw', height: '100vh' }}>
+        <SidePanelWrap>
+          <SidePanel store={store} />
+        </SidePanelWrap>
+        <WorkspaceWrap>
+          <Toolbar store={store} downloadButtonEnabled />
+          <Workspace store={store} />
+          <ZoomButtons store={store} />
+          <PagesTimeline store={store} />
+        </WorkspaceWrap>
+      </PolotnoContainer>
+    </>
   );
 };
 
